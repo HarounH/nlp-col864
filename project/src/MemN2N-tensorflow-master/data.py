570228163726a61c865ce65fe256073acdb42ec1
Haroun H <haroun7@gmail.com>
2017-03-16 18:09:46 +0530

    debugging baseline

--- project/src/MemN2N-tensorflow-master/data.py
+++ project/src/MemN2N-tensorflow-master/data.py
@@ -1,36 +1,105 @@
+from __future__ import absolute_import
+
 import os
+import re
+import numpy as np
 from collections import Counter
 
 def read_data(fname, count, word2idx):
-    if os.path.isfile(fname):
-        with open(fname) as f:
-            lines = f.readlines()
-    else:
-        raise("[!] Data %s not found" % fname)
+	if os.path.isfile(fname):
+		with open(fname) as f:
+			lines = f.readlines()
+	else:
+		raise("[!] Data %s not found" % fname)
 
-    words = []
-    for line in lines:
-        words.extend(line.split())
+	words = []
+	for line in lines:
+		words.extend(line.split())
 
-    if len(count) == 0:
-        count.append(['<eos>', 0])
+	if len(count) == 0:
+		count.append(['<eos>', 0])
 
-    count[0][1] += len(lines)
-    count.extend(Counter(words).most_common())
+	count[0][1] += len(lines)
+	count.extend(Counter(words).most_common())
 
-    if len(word2idx) == 0:
-        word2idx['<eos>'] = 0
+	if len(word2idx) == 0:
+		word2idx['<eos>'] = 0
 
-    for word, _ in count:
-        if word not in word2idx:
-            word2idx[word] = len(word2idx)
+	for word, _ in count:
+		if word not in word2idx:
+			word2idx[word] = len(word2idx)
 
-    data = list()
-    for line in lines:
-        for word in line.split():
-            index = word2idx[word]
-            data.append(index)
-        data.append(word2idx['<eos>'])
+	data = list()
+	for line in lines:
+		for word in line.split():
+			index = word2idx[word]
+			data.append(index)
+		data.append(word2idx['<eos>'])
 
-    print("Read %s words from %s" % (len(data), fname))
-    return data
+	print("Read %s words from %s" % (len(data), fname))
+	return data
+
+def read_dstc2_data(fname):
+	print('Reading ', fname)
+	data=[]
+	with open(fname) as f:
+		mem = []
+		for line in f.readlines():
+			if line.strip():
+				nid, line = line.split(' ', 1)
+				nid = int(nid)
+				line = line.rstrip()
+				if '\t' in line:
+					q,a= line.split('\t',1)
+					q = q.split(' ')
+					data.append((mem[:], q, a))
+					mem.append(q)
+					mem.append(a.split(' '))
+				else:
+					mem.append(line.split(' '))
+			else:
+				mem = []
+	return data
+
+def vectorize_data(raw_data, max_sentence_length, n_memory_cells, word2idx, candidate2idx):
+	'''
+		Takes in raw_data and converts it into vectorised form depending on word2idx, candidate2idx
+
+		max_sentence_length also accomodates for time and utterer .
+	'''
+	contexts = []
+	queries = []
+	answers = []
+
+	for context, query, answer in raw_data:
+		c = [] # vectorised context
+		# q = [] # vectorised query
+		# a = [] # vectorised answer
+
+		# sentence is a list of words.
+		for i, sentence in enumerate(context, 1): # What is enumerate(context,1)?? :P
+			padding_size = max(0, max_sentence_length - len(sentence)) 
+			c.append([ word2idx[w] for w in sentence ] + [0]*padding_size)
+			c[-1] = 1 + i%2 # utterer Either system or user.... not 0 because 0 means empty for me.
+
+
+		# Ignore really old sentences that don't fit in memory.
+		c = c[::-1][:n_memory_cells][::-1]
+		for i in range(len(c)):
+			c[i][-2] = len(word2idx) - n_memory_cells - i + len(c) # Time ... NOTE: This might have to be different.
+
+		n_empty_cells = max(0, n_memory_cells - len(c))
+		for _ in range(n_empty_cells):
+			c.append([0]*max_sentence_length) # Empty memory.
+
+		lq = max(0, max_sentence_length - len(query)) # >=0 
+		q = [word2idx[w] for w in query] + [0] * lq # Padded with 0s. 
+
+		a = np.zeros(len(candidate2idx)) # 0 is reserved for nil word
+		a[candidate2idx[answer]] = 1
+
+		contexts.append(c)
+		queries.append(q)
+		answers.append(a)
+
+	return np.array(contexts), np.array(queries), np.array(answers)
--- project/src/MemN2N-tensorflow-master/main.py
+++ project/src/MemN2N-tensorflow-master/main.py
@@ -1,16 +1,19 @@
 import os
 import pprint
+import numpy as np
 import tensorflow as tf
 
-from data import read_data
+from data import read_data, read_dstc2_data, vectorize_data
 from model import MemN2N
+from itertools import chain
+from six.moves import range, reduce
 
 pp = pprint.PrettyPrinter()
 
 flags = tf.app.flags
 
 flags.DEFINE_integer("edim", 150, "internal state dimension [150]")
-flags.DEFINE_integer("lindim", 75, "linear part of the state [75]")
+flags.DEFINE_integer("lindim", 10, "linear part of the state [75]")
 flags.DEFINE_integer("nhop", 6, "number of hops [6]")
 flags.DEFINE_integer("mem_size", 100, "memory size [100]")
 flags.DEFINE_integer("batch_size", 128, "batch size to use during training [128]")
@@ -19,38 +22,79 @@ flags.DEFINE_float("init_lr", 0.01, "initial learning rate [0.01]")
 flags.DEFINE_float("init_hid", 0.1, "initial internal state value [0.1]")
 flags.DEFINE_float("init_std", 0.05, "weight initialization std [0.05]")
 flags.DEFINE_float("max_grad_norm", 50, "clip gradients to this norm [50]")
-flags.DEFINE_string("data_dir", "data", "data directory [data]")
+flags.DEFINE_string("data_dir", "../weston_baseline/data/dialog-bAbI-tasks", "data directory [data]")
 flags.DEFINE_string("checkpoint_dir", "checkpoints", "checkpoint directory [checkpoints]")
-flags.DEFINE_string("data_name", "ptb", "data set name [ptb]")
+# flags.DEFINE_string("data_name", "ptb", "data set name [ptb]")
+flags.DEFINE_string("data_name", "dialog-babi-task6-dstc2", "data set name [dialog-babi-task6-dstc2]")
 flags.DEFINE_boolean("is_test", False, "True for testing, False for Training [False]")
 flags.DEFINE_boolean("show", False, "print progress [False]")
-
+flags.DEFINE_string("candidate_filename","dialog-babi-task6-dstc2-candidates.txt","file containing valid candidates")
 FLAGS = flags.FLAGS
 
 def main(_):
-    count = []
-    word2idx = {}
+	count = []
+	word2idx = {}
 
-    if not os.path.exists(FLAGS.checkpoint_dir):
-      os.makedirs(FLAGS.checkpoint_dir)
+	if not os.path.exists(FLAGS.checkpoint_dir):
+	  os.makedirs(FLAGS.checkpoint_dir)
+	'''
+	train_data = read_data('%s/%s-trn.txt' % (FLAGS.data_dir, FLAGS.data_name), count, word2idx)
+	valid_data = read_data('%s/%s-dev.txt' % (FLAGS.data_dir, FLAGS.data_name), count, word2idx)
+	test_data = read_data('%s/%s-tst.txt' % (FLAGS.data_dir, FLAGS.data_name), count, word2idx)
+	'''
+	raw_train = read_dstc2_data('%s/%s-trn.txt' % (FLAGS.data_dir, FLAGS.data_name))
+	raw_test = read_dstc2_data('%s/%s-tst.txt' % (FLAGS.data_dir, FLAGS.data_name))
+	raw_val = read_dstc2_data('%s/%s-dev.txt' % (FLAGS.data_dir, FLAGS.data_name))
 
-    train_data = read_data('%s/%s.train.txt' % (FLAGS.data_dir, FLAGS.data_name), count, word2idx)
-    valid_data = read_data('%s/%s.valid.txt' % (FLAGS.data_dir, FLAGS.data_name), count, word2idx)
-    test_data = read_data('%s/%s.test.txt' % (FLAGS.data_dir, FLAGS.data_name), count, word2idx)
+	raw_data = raw_train + raw_test + raw_val
 
-    idx2word = dict(zip(word2idx.values(), word2idx.keys()))
-    FLAGS.nwords = len(word2idx)
+	vocab = sorted(reduce(lambda x, y: x | y, (set(list(chain.from_iterable(s)) + q) for s, q, a in raw_data)))
+	print(len(vocab))
+	word2idx = dict((c, i + 1) for i, c in enumerate(vocab))
 
-    pp.pprint(flags.FLAGS.__flags)
+	idx=1
+	candidate2idx={}
+	with open('%s/%s' % (FLAGS.data_dir, FLAGS.candidate_filename)) as cFile:
+		for line in cFile.readlines():
+			nid, line = line.split(' ', 1)
+			candidate2idx[line.rstrip()]=idx
+			idx=idx+1
 
-    with tf.Session() as sess:
-        model = MemN2N(FLAGS, sess)
-        model.build_model()
+	max_story_size = max(map(len, (s for s, _, _ in raw_data)))
+	mean_story_size = int(np.mean([ len(s) for s, _, _ in raw_data ]))
+	sentence_size = max(map(len, chain.from_iterable(s for s, _, _ in raw_data)))
+	query_size = max(map(len, (q for _, q, _ in raw_data)))
+	n_memory_cells = min(FLAGS.lindim, max_story_size)
+	answer_size = len(candidate2idx) # +1 TODO ... do we need +1???
 
-        if FLAGS.is_test:
-            model.run(valid_data, test_data)
-        else:
-            model.run(train_data, valid_data)
+	# We now have the candidates and everything else.
+
+	idx2word = dict(zip(word2idx.values(), word2idx.keys()))
+	FLAGS.nwords = len(word2idx)
+
+
+	sentence_size = max(query_size, sentence_size) # for the position
+	sentence_size += 2  # +1 for time words, +1 for utterer
+	max_sentence_length = sentence_size
+
+	# Vectorise data.
+	train_data = vectorize_data(raw_train, max_sentence_length, n_memory_cells, word2idx, candidate2idx)
+	test_data = vectorize_data(raw_test, max_sentence_length, n_memory_cells, word2idx, candidate2idx)
+	val_data = vectorize_data(raw_val, max_sentence_length, n_memory_cells, word2idx, candidate2idx)
+
+	print('Done loading data')
+
+	pp.pprint(flags.FLAGS.__flags)
+
+	# Do model things.
+	with tf.Session() as sess:
+		model = MemN2N(FLAGS, sess)
+		model.build_model()
+
+		if FLAGS.is_test:
+			model.run(valid_data, test_data)
+		else:
+			model.run(train_data, valid_data)
 
 if __name__ == '__main__':
-    tf.app.run()
+	tf.app.run()
